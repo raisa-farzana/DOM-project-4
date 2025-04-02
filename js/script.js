276d671bd130888551b7f7456bd620d2abf23dd6
raisa-farzana <raisafarzana5502@gmail.com>
2025-04-02 23:50:50 +0600

    updated js

--- js/script.js
+++ js/script.js
@@ -2,10 +2,21 @@ const extractText = () => {
     const fileInput = document.getElementById('imageInput');
     const outPutDiv = document.getElementById('output');
 
-    const imagesFile = fileInput.isDefaultNamespace[0];
+    const imagesFile = fileInput.files[0];
 
     if (!imagesFile) {
         outPutDiv.textContent = 'Please select an image file.'
         return;
     }
+
+    Tesseract.recognize(
+        imagesFile,
+        'eng'
+
+    ).then(({data}) => {
+        outPutDiv.textContent = data.text;
+    }).catch( (error) => {
+        console.error('Error', error);
+        outPutDiv.textContent ='Error';
+    })
 }
